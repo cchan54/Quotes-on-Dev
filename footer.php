8293d222e86ab17c210beb65c475c8ce68f2255e
Cliff Chan <cchan17@live.ca>
2018-02-28 11:20:09 -0800

    changed page submit

--- footer.php
+++ footer.php
@@ -11,7 +11,19 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'primary',
+							'menu_id' => 'primary-menu',
+							'menu_class' => 'footer-navigation'
+						)
+						);
+						?>
+
+					<p>Brought to you by © <a href="http://www.cliffchan.ca">Cliff Chan</a> <?php echo date( 'Y' ) ?></p>
+
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
